a54e49c39f212fcb828c5baa51455328676374b3
Huuhao1712 <nhhao21@apcs.fitus.edu.vn>
2023-12-13 14:42:48 +0700

    L6

--- section5.py
+++ section5.py
@@ -2,13 +2,18 @@ from section2 import extended_euclidean
 
 from math import sqrt
 
+import math
+
 def modularSquareRoot(a, p):
     for i in range(1, p):
         if i * i % p == a:
             return i
     return None
 
-#print(modularSquareRoot(448, 2019**2))
+print("modular")
+print(modularSquareRoot(1983, 2027))
+print(modularSquareRoot(873, 2029))
+print(modularSquareRoot(474993, 1003001))
 
 def chinesesRemainder(n, a):
     sum = 0
@@ -28,7 +33,7 @@ def modularInverse(a, m):
             return i
     return None
 
-print(pow(9, 9**9, 1001))
+#print(pow(9, 9**9, 1001))
 
 def noneffectiveFermat(p):
     for i in range (1, int(sqrt(p))):
@@ -37,4 +42,42 @@ def noneffectiveFermat(p):
             return i, int(q)
     return None
 
-print(noneffectiveFermat(252497801))
+#print(noneffectiveFermat(252497801))
+
+def float_to_fraction (x, error=0.00001):
+    n = int(math.floor(x))
+    x -= n
+    if x < error:
+        return (n, 1)
+    elif 1 - error < x:
+        return (n+1, 1)
+
+    # The lower fraction is 0/1
+    lower_n = 0
+    lower_d = 1
+    # The upper fraction is 1/1
+    upper_n = 1
+    upper_d = 1
+    while True:
+        # The middle fraction is (lower_n + upper_n) / (lower_d + upper_d)
+        middle_n = lower_n + upper_n
+        middle_d = lower_d + upper_d
+        # If x + error < middle
+        if middle_d * (x + error) < middle_n:
+            # middle is our new upper
+            upper_n = middle_n
+            upper_d = middle_d
+        # Else If middle < x - error
+        elif middle_n < (x - error) * middle_d:
+            # middle is our new lower
+            lower_n = middle_n
+            lower_d = middle_d
+        # Else middle is our best fraction
+        else:
+            return (n * middle_d + middle_n, middle_d)
+        
+print("L6")
+
+print(float_to_fraction(0.372636))
+print(float_to_fraction(0.373346671))
+print(float_to_fraction(0.2173836482))
